8acaaf4366447fe040db445cccf2f6705b60e20a
ssportplus <ridvan.akpunar@ssportplus.com>
2021-05-27 00:00:12 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+from videoprops import get_video_properties
+nameList = os.listdir()
+props = get_video_properties
+for name in nameList:
+    if name[-3:] == "mp4":
+        print(f'{name}\n{props(name)["height"]}p')
