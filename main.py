5a169a55249df8c2a90f51f9366bcd3b2f5b1f98
ssportplus <ridvan.akpunar@ssportplus.com>
2021-05-29 08:43:28 +0300

    Update main.py
    Add mkv support

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from videoprops import get_video_properties
 nameList = os.listdir()
 props = get_video_properties
 for name in nameList:
-    if name[-3:] == "mp4":
+    if name[-3:] == "mp4" or name[-3:] == "mkv":
         print(f'{name}\n{props(name)["height"]}p')
